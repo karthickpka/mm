c22cae064e1f55fe5f2520203e8c49e4ebd89344
Karthick Palanisamy <kapapala@oradev.oraclecorp.com>
2018-06-07 11:35:37 +0530

    session management

--- Client/Js/Authenticate.js
+++ Client/Js/Authenticate.js
@@ -1,31 +1,24 @@
-function pwd()
-{
-  if(document.getElementById("Password").value=="123" ){
-      document.getElementById("Edit").disabled = false;
-      document.getElementById("Delete").disabled = false;}
-  else{
-    document.getElementById("Edit").disabled = true;
-    document.getElementById("Delete").disabled = true;}
+function pwd() {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", '/username', false); // false for synchronous request
+  xmlHttp.send();
+
+  if (xmlHttp.responseText == "shop1") {
+    document.getElementById('ShopName').value = "Shop1";
+  }
+  else if (xmlHttp.responseText == "shop2") {
+    document.getElementById('ShopName').value = "Shop2";
+  }
+  else if (xmlHttp.responseText == "shop3") {
+    document.getElementById('ShopName').value = "Shop3";
+  }
+  else if (xmlHttp.responseText == "admin") {
+    document.getElementById('ShopName').value = "All";
+    document.getElementById('Edit').disabled = false;
+    document.getElementById('Delete').disabled = false;
+    document.getElementById('ShopName').disabled = false;
+  }
+  else {
+    window.location.replace('/');  //repalce will remove from browser back history
+  }
 }
-//http://localhost:4444/insertRecord?imei=1&model=1&mrp=1&mop=1&discount=1&comment=xyz
- /*if(document.getElementById("IMEI").value)
-    {
-    queryString  = "/insertRecord?imei="+document.getElementById("IMEI").value +
-                    "&date="+document.getElementById("Date").value +
-                    "&model="+document.getElementById("Model").value +
-                    "&mrp="+document.getElementById("MRP").value +
-                    "&mop="+document.getElementById("MOP").value +
-                    "&discount="+document.getElementById("Discount").value +
-                    "&comment="+document.getElementById("Comment").value +
-                    "&avail="+document.getElementById("Availability").value
-    
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", queryString, false ); // false for synchronous request
-        xmlHttp.send();
-        alert(xmlHttp.responseText);
-        viewAll();
-    }
-  else
-  {alert("Ener atleast IMEI number before trying to ADD")}
-}
-*/
